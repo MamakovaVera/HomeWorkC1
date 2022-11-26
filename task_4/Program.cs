83f8af7128e3b2f85582b6fa905505cf0f862d35
Vera <mvp2283@mail.ru>
2022-11-26 12:31:56 +0600

    task4

---
+++ task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.clear();
+
+Console.Write("Введите 1е число: ");
+int a = ConvertToInt32(Console.ReadLint());
+
+Console.Write("Введите 2е число: ");
+int b = ConvertToInt32(Console.ReadLint());
+
+Console.Write("Введите 2е число: ");
+int c = ConvertToInt32(Console.ReadLint());
+
+if (a >=b $$ a >= c)
+    Console.Write(a);
+else (b >=a $$ b>=c)
+    Console.Write(b);
+else
+    Console.Write(c);
