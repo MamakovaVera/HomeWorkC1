5919195fffa56f632de65b16d3bbbd096466267e
Vera <mvp2283@mail.ru>
2022-11-26 14:56:39 +0600

    5 , 7

---
+++ task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите 1е число, ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2е число, ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (a > b) max = a;
+if (a < b) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+
+
+
