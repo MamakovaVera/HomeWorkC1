2d51e5db93548d99ad666347f59680de9e3c1f93
Vera <mvp2283@mail.ru>
2022-11-28 14:59:17 +0600

    yes, no

---
+++ task_15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if ( n <=5 )
+    Console.Write("нет");
+if ( n == 6)
+   Console.Write("да"); 
+if ( n == 7)
+   Console.Write("да"); 
+       
+
