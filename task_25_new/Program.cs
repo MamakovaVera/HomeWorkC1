56c6a2d7758b30367564c3e3c0040f4a6673466e
Vera <mvp2283@mail.ru>
2022-12-06 11:54:21 +0600

    a в степени b

---
+++ task_25_new/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 25: Напишите цикл, который принимает 
+//на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+Console.Clear();
+Console.Write("Введите 1е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int count = 1; count <= b; count++ )
+{
+   result = result * a;
+}
+
+Console.Write(result);
