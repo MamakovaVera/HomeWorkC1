7d4a5e2d9434375394b3fd78d611d1f88a80898f
Vera <mvp2283@mail.ru>
2022-12-08 00:01:04 +0600

    сумма чисел

---
+++ task_27_3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе. 452 -> 11, 82 -> 10, 9012 -> 12
+Console.Clear();
+Console.Write("Введите число: ");
+string? n = Console.ReadLine();
+int result = 0;
+for ( int i = 0 ; i < n.Length; i++)
+    result = result + int.Parse(n[i].ToString());
+Console.WriteLine(result);
