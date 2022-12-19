7022849b5d768806d746e4eae596b187725a22f3
Vera <mvp2283@mail.ru>
2022-12-19 22:31:41 +0600

    factorial N

---
+++ task_pr9_recurs_N!/Program.cs
@@ -0,0 +1,13 @@
+﻿// нахождение факториала числа N, через рекурсию
+
+int sum(int n)
+{
+    Console.WriteLine($"{n} ");
+    if (n == 0)
+    return 1;
+    return sum(n - 1) * n;
+}
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(n));
