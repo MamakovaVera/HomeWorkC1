c198d6e72e7a550278808fd640d27275faf5115b
Vera <mvp2283@mail.ru>
2022-12-19 22:16:43 +0600

    sum

---
+++ task_pr9_recersia1/Program.cs
@@ -0,0 +1,14 @@
+﻿/* задача найти сумму без сложения. через рекурсию*/
+
+
+int sum(int n, int m)
+{
+if (m == 0)
+return n;
+return sum(n + 1, m - 1);
+}
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(n, m));
