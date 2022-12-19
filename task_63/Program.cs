a821916fde80c331928c132203c0f19c1e635222
Vera <mvp2283@mail.ru>
2022-12-19 23:06:49 +0600

    последовательное выведение

--- task_63/Program.cs
+++ task_63/Program.cs
@@ -3,7 +3,7 @@
 N = 5 -> "1, 2, 3, 4, 5"
 N = 6 -> "1, 2, 3, 4, 5, 6"
 */
-string rec(int n)
+string rec(int n) //решение через рекурсию
 {
     if (n == 1)
         return "1 ";
@@ -16,6 +16,23 @@ Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(rec(n));
 
+/*void rec(int n) //решение через поцедуру
+{
+    if (n == 1)
+        Console.Write(n + " ");
+    if (n > 1)
+{
+    rec(n - 1); // вывод будет 1, 2, 3, 4....10
+    Console.Write(n + " ");
+    //rec(n - 1); // если записать строку здесь, вывод будет 10, 9, 8....1
+}
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+rec(n);
+*/
 /*int rec(int n) 
 {
     if (n == 1)
