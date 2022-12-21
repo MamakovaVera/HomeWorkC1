db92af2a4d6e98aae7626e4fb4cc8179fff4bdca
Vera <mvp2283@mail.ru>
2022-12-21 15:47:30 +0600

    проиведение матриц

--- task_58/Program.cs
+++ task_58/Program.cs
@@ -6,7 +6,55 @@
 18 20
 15 18
 */
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+    }
+}
 
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+int[,] ReleaseMatrix(int[,] FirstMatrix, int[,] SecondMatrix)
+{
+    int[,] result = new int[FirstMatrix.GetLength(0), FirstMatrix.GetLength(1)];
+    for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+            result[i, j] = FirstMatrix[i, j] * SecondMatrix[i, j];
+    }
+    return result;
+}
+
+
+Console.Clear();
+Console.Write("Введите размер матриц: "); // размеры матриц должны быть одинаковые
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] FirstMatrix = new int[size[0], size[1]];
+int[,] SecondMatrix = new int[size[0], size[1]];
+InputMatrix(FirstMatrix);
+InputMatrix(SecondMatrix);
+Console.WriteLine("Начальный массив 1: ");
+PrintMatrix(FirstMatrix);
+Console.WriteLine("Начальный массив 2: ");
+PrintMatrix(SecondMatrix);
+Console.WriteLine("Результат:");
+PrintMatrix(ReleaseMatrix(FirstMatrix, SecondMatrix));
+
+
+/*
 Console.Clear();
 int m = InputNumbers("Введите число строк 1-й матрицы: ");
 int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
@@ -73,3 +121,4 @@ void WriteArray (int[,] array)
     Console.WriteLine();
   }
 }
+*/
