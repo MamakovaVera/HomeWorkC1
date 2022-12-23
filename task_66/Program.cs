24e193c67a49e941a62d741efdc876fdc1c1771e
Vera <mvp2283@mail.ru>
2022-12-23 13:47:56 +0600

    сумма через рекурсию

---
+++ task_66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+//сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+ Console.Clear();
+Console.Write("Введите первое чило: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе чило: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int SumNaturalNumbers(int n, int m)
+{
+  if(n > m)
+  {
+     return 0;
+  }else
+return n + SumNaturalNumbers(n + 1, m) ;
+}
+//SumNaturalNumbers(n, m);
+Console.WriteLine($"Сумма = {SumNaturalNumbers(n, m)}");
+
+     
